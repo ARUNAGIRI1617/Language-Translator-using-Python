e926f29aa4d62b49501b901f83c1d64328273cbf
MR.ROBOT <89141198+ARUNAGIRI1617@users.noreply.github.com>
2022-06-26 20:19:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ st.title("Language Translator")
 
 text = st.text_input("Enter a text")
 
-translate = translator.translate(text, lang_tgt='de')# you can give any country code eg:"fr","en","th",etc...
+translate = translator.translate(text, lang_tgt='de')# You can give any country code eg:"fr","en","th",etc...
 
-st.write(translate)
+st.write(translate)#Visit your localhost to see the translator.
