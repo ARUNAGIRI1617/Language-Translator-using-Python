892243974960ae9569bcd77e13b72022be6f1d56
MR.ROBOT <89141198+ARUNAGIRI1617@users.noreply.github.com>
2022-06-26 20:17:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#Translator
+from google_trans_new import google_translator
 
+import streamlit as st
+
+translator = google_translator()
+
+st.title("Language Translator")
+
+text = st.text_input("Enter a text")
+
+translate = translator.translate(text, lang_tgt='de')# you can give any country code eg:"fr","en","th",etc...
+
+st.write(translate)
